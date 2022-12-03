73db21207bd173f7f429a11b61df0cc002e9b50f
Ryan Bohr <ryanbohr42@hotmail.com>
2022-12-03 00:45:53 -0600

    Day 1 part 1 solution

---
+++ Day1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Day1{
+    public static void main(String[] args) throws FileNotFoundException{
+        
+        File file = new File("ElfCals.txt");
+        Scanner input = new Scanner(file);
+        
+        int mostCals = -1;
+        int sum = 0;
+        while(input.hasNext()){
+            String line = input.nextLine();
+            //System.out.println(line);
+            if(line.isEmpty()){
+               sum = 0; 
+            }
+            else{
+                sum += Integer.parseInt(line);
+                if(sum > mostCals)
+                    mostCals = sum;
+            }
+        }
+
+        System.out.println(mostCals);
+        input.close();
+    }
+}
